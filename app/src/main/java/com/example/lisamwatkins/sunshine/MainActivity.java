97dc02de5847badfc92279a0a931e9ce64ba3e0f
lisamariewatkins <lisamwatkins@ymail.com>
2017-08-25 21:52:14 -0500

    hooked up toudacity api

--- app/src/main/java/com/example/lisamwatkins/sunshine/MainActivity.java
+++ app/src/main/java/com/example/lisamwatkins/sunshine/MainActivity.java
@@ -1,37 +1,62 @@
 package com.example.lisamwatkins.sunshine;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.example.lisamwatkins.sunshine.data.SunshinePreferences;
+import com.example.lisamwatkins.sunshine.utilities.NetworkUtils;
+import com.example.lisamwatkins.sunshine.utilities.OpenWeatherJsonUtils;
+import com.example.lisamwatkins.sunshine.utilities.SunshineWeatherUtils;
+
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
-    private TextView weatherView;
+    private TextView mWeatherView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
 
-        weatherView = (TextView) findViewById(R.id.tv_weather_data);
+        mWeatherView = (TextView) findViewById(R.id.tv_weather_data);
+    }
 
-        String[] dummyWeatherData = {
-                "Today, May 17 - Clear - 17°C / 15°C",
-                "Tomorrow - Cloudy - 19°C / 15°C",
-                "Thursday - Rainy- 30°C / 11°C",
-                "Friday - Thunderstorms - 21°C / 9°C",
-                "Saturday - Thunderstorms - 16°C / 7°C",
-                "Sunday - Rainy - 16°C / 8°C",
-                "Monday - Partly Cloudy - 15°C / 10°C",
-                "Tue, May 24 - Meatballs - 16°C / 18°C",
-                "Wed, May 25 - Cloudy - 19°C / 15°C",
-                "Thu, May 26 - Stormy - 30°C / 11°C",
-                "Fri, May 27 - Hurricane - 21°C / 9°C",
-                "Sat, May 28 - Meteors - 16°C / 7°C",
-                "Sun, May 29 - Apocalypse - 16°C / 8°C",
-                "Mon, May 30 - Post Apocalypse - 15°C / 10°C",
-        };
+    private void loadWeatherData(){
+        String location = SunshinePreferences.getPreferredWeatherLocation(this);
+        new FetchWeatherTask().execute(location);
+    }
 
-        for(String data : dummyWeatherData){
-            weatherView.append(data + "\n\n\n");
+    public class FetchWeatherTask extends AsyncTask<String, Void, String[]>{
+        @Override
+        protected String[] doInBackground(String... params) {
+            if (params.length == 0){
+                return null;
+            }
+
+            String location = params[0];
+
+            URL weatherRequest = NetworkUtils.buildUrl(location);
+
+            try{
+                String jsonWeatherResponse = NetworkUtils.getResponseFromHttpUrl(weatherRequest);
+                String[] simpleJsonWeatherData = OpenWeatherJsonUtils
+                        .getSimpleWeatherStringsFromJson(MainActivity.this, jsonWeatherResponse);
+
+                return simpleJsonWeatherData;
+            }catch (Exception e){
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String[] weatherData) {
+            if(weatherData != null){
+                for (String weather : weatherData){
+                    mWeatherView.append(weather + "\n\n\n");
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/lisamwatkins/sunshine/utilities/NetworkUtils.java
+++ app/src/main/java/com/example/lisamwatkins/sunshine/utilities/NetworkUtils.java
@@ -15,9 +15,12 @@
  */
 package com.example.lisamwatkins.sunshine.utilities;
 
+import android.net.Uri;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
@@ -65,8 +68,19 @@ public final class NetworkUtils {
      * @return The URL to use to query the weather server.
      */
     public static URL buildUrl(String locationQuery) {
-        /** This will be implemented in a future lesson **/
-        return null;
+        Uri buildUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                .appendQueryParameter(QUERY_PARAM, locationQuery)
+                .appendQueryParameter(FORMAT_PARAM, format)
+                .appendQueryParameter(UNITS_PARAM, units)
+                .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
+                .build();
+        URL builtUrl = null;
+        try{
+            builtUrl = new URL(buildUri.toString());
+        } catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+        return builtUrl;
     }
 
     /**
