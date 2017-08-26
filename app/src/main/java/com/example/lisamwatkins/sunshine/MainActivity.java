2a0df9f5ca69cc33ddaefbf66c4e6e7f01f173a5
lisamariewatkins <lisamwatkins@ymail.com>
2017-08-26 11:26:06 -0500

    added error and progress bar views

--- app/src/main/java/com/example/lisamwatkins/sunshine/MainActivity.java
+++ app/src/main/java/com/example/lisamwatkins/sunshine/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.lisamwatkins.sunshine.data.SunshinePreferences;
@@ -13,16 +15,25 @@ import com.example.lisamwatkins.sunshine.utilities.NetworkUtils;
 import com.example.lisamwatkins.sunshine.utilities.OpenWeatherJsonUtils;
 import com.example.lisamwatkins.sunshine.utilities.SunshineWeatherUtils;
 
+import org.w3c.dom.Text;
+
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mWeatherView;
+    private TextView mErrorMessageTextView;
+    private ProgressBar mLoadingProgressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
 
         mWeatherView = (TextView) findViewById(R.id.tv_weather_data);
+        mErrorMessageTextView = (TextView) findViewById(R.id.error_message);
+        mLoadingProgressBar = (ProgressBar) findViewById(R.id.loading_progress);
+
+        loadWeatherData();
     }
 
     private void loadWeatherData(){
@@ -30,7 +41,23 @@ public class MainActivity extends AppCompatActivity {
         new FetchWeatherTask().execute(location);
     }
 
+    private void showWeatherDataView(){
+        mErrorMessageTextView.setVisibility(View.INVISIBLE);
+        mWeatherView.setVisibility(View.VISIBLE);
+    }
+
+    private void showErrorView(){
+        mErrorMessageTextView.setVisibility(View.VISIBLE);
+        mWeatherView.setVisibility(View.INVISIBLE);
+    }
+
     public class FetchWeatherTask extends AsyncTask<String, Void, String[]>{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingProgressBar.setVisibility(View.VISIBLE);
+        }
+
         @Override
         protected String[] doInBackground(String... params) {
             if (params.length == 0){
@@ -55,10 +82,16 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String[] weatherData) {
+            mLoadingProgressBar.setVisibility(View.INVISIBLE);
+
             if(weatherData != null){
+                showWeatherDataView();
                 for (String weather : weatherData){
                     mWeatherView.append(weather + "\n\n\n");
                 }
+            }
+            else{
+                showErrorView();
             }
         }
     }
